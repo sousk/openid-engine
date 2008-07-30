e326f7b6fc95b45eacb384c237fe16f8f731ea85
sousk <sou.phphp@gmail.com>
2008-07-30 10:01:46 +0900

    add new funtion to generate random chars. printable chars only by default

--- lib/openid_engine.rb
+++ lib/openid_engine.rb
@@ -6,6 +6,13 @@ module OpenidEngine
     :identifier_select => 'http://specs.openid.net/auth/2.0/identifier_select'
   }
   
+  def random_strings(size, range=[33,126])
+    source = (range[0]..range[1]).to_a
+    (0...size).collect {
+      source[Kernel.rand(source.size)].chr
+    }.join
+  end
+  
   class Error < StandardError
   end
 end
--- spec/openid_engine_spec.rb
+++ spec/openid_engine_spec.rb
@@ -3,8 +3,14 @@ require File.dirname(__FILE__) + '/spec_helper'
 describe 'OpenidEngine' do
   include OpenidEngine
   
-  it {
-    OpenidEngine::TYPE[:server].should_not be_empty
-  }
+  describe "#random_strings" do
+    it "should be the length specified" do
+      random_strings(32).size.should == 32
+    end
+    
+    it "should consist of chars specified" do
+      random_strings(24, [33,33]).should == 33.chr * 24 # should be 
+    end
+  end
 
 end
